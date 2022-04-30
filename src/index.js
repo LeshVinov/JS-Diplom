a51492ea1028f9e17ff96935987f19df88180205
LeshVinov <alekse.03@mail.ru>
2022-04-30 21:01:34 +0300

    add: program files, 3 modules added

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import modal from "./modules/modal"
+import slider from "./modules/slider"
+import scroll from "./modules/smoothScrollBtn"
+import timer from "./modules/timer"
+
+modal()
+slider()
+scroll()
+timer('05 may 2022')
