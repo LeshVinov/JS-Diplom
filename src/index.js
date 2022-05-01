665fc8fda366fb8940632892bfd7cdb39962c9d2
LeshVinov <alekse.03@mail.ru>
2022-05-01 14:53:57 +0300

    add: burger button added and done

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import modal from "./modules/modal"
 import slider from "./modules/slider"
 import scroll from "./modules/smoothScrollBtn"
 import timer from "./modules/timer"
+import burger from "./modules/burger"
 
 modal()
 slider()
 scroll()
+burger()
 timer('05 may 2022')
