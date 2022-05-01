b7edc2e95edd9f885d3ba14dba5dfa1a6b31f697
LeshVinov <alekse.03@mail.ru>
2022-05-01 16:27:01 +0300

    add: Form validation added and done

--- src/index.js
+++ src/index.js
@@ -3,9 +3,28 @@ import slider from "./modules/slider"
 import scroll from "./modules/smoothScrollBtn"
 import timer from "./modules/timer"
 import burger from "./modules/burger"
+import formValidate from "./modules/formValidate"
+import sendForm from "./modules/sendForm"
 
 modal()
 slider()
 scroll()
 burger()
+// formValidate()
 timer('05 may 2022')
+sendForm({
+    formId: '[name = "action-form"]', 
+    someElem: [
+        {
+            type: 'block',
+        }
+    ] 
+})
+sendForm({
+    formId: '[name = "action-form2"]', 
+    someElem: [
+        {
+            type: 'block',
+        }
+    ] 
+})
